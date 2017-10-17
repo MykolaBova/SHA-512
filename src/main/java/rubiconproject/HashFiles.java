f0492ac16e7e4af53d12f6deb68d771776e765a5
rublin <toor.ua@gmail.com>
2017-10-17 20:34:05 +0300

    Initial commit

---
+++ src/main/java/rubiconproject/HashFiles.java
@@ -0,0 +1,16 @@
+package rubiconproject;
+
+/**
+ * Copyright © 2016 Rubicon Project, All rights reserved.
+ */
+public class HashFiles {
+
+    public static void main(String[] argv) {
+        if (argv.length < 1) {
+            System.err.println("one arguments expected: <path>");
+            System.exit(1);
+        }
+
+        // Implement a program to calculate hash of the specified directory or file
+    }
+}
