41a14ece8fe2a7e77bbf655401449844e9ca5a36
rublin <toor.ua@gmail.com>
2017-10-17 21:31:33 +0300

    add logging

--- src/main/java/rubiconproject/hash/Sha512HashGenerator.java
+++ src/main/java/rubiconproject/hash/Sha512HashGenerator.java
@@ -1,17 +1,21 @@
 package rubiconproject.hash;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Sha512HashGenerator implements HashGenerator {
+    private static final Logger LOG = LogManager.getLogger(Sha512HashGenerator.class);
+    private static final String ALGORITHM = "SHA-512";
 
     @Override
     public String getHash(String string) {
-        String hashedString = null;
+        String hashedString = "";
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-//            md.update(salt.getBytes("UTF-8"));
+            MessageDigest md = MessageDigest.getInstance(ALGORITHM);
             byte[] bytes = md.digest(string.getBytes("UTF-8"));
             StringBuilder sb = new StringBuilder();
             for(int i=0; i< bytes.length ;i++){
@@ -20,9 +24,9 @@ public class Sha512HashGenerator implements HashGenerator {
             hashedString = sb.toString();
         }
         catch (NoSuchAlgorithmException e){
-            e.printStackTrace();
+            LOG.error("Hash algorithm error: {}", e.getMessage());
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            LOG.error("Encoding error: {}", e.getMessage());
         }
         return hashedString;
     }
